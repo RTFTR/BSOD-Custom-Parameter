bca1fd0fc65e3a167df3d02b3733ce934968cb2f
RTFTR <83257329+RTFTR@users.noreply.github.com>
2024-05-07 13:09:24 +0900

    Deleting useless C header file

--- bsod/bsod_nt.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include<tchar.h>
-#include <Windows.h>
-#include <winternl.h>
-#pragma comment(lib, "ntdll.lib")
-
-extern "C" NTSTATUS NTAPI RtlAdjustPrivilege(ULONG Privilege, BOOLEAN Enable, BOOLEAN CurrentThread, PBOOLEAN OldValue);
-extern "C" NTSTATUS NTAPI NtRaiseHardError(LONG ErrorStatus, ULONG NumberOfParameters, PUNICODE_STRING UnicodeStringParameterMask, PULONG_PTR Parameters, ULONG ValidResponseOptions, PULONG Response);
